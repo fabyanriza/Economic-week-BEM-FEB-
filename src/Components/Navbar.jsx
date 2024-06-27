776de2f794c203411e2732b3c315e9ef9cef9484
unknown <fabyanriza67@gmail.com>
2024-06-28 01:20:52 +0700

    navbar sudah responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,23 +3,23 @@ import Logo from "../assets/Logo.png";
 
 function Navbar() {
   return (
-    <div className="bg-custom-gradient max-w-screen h-20 flex justify-center">
+    <div className="bg-custom-gradient  h-20 flex justify-center">
       <div className="flex justify-center items-center w-1/2 h-auto gap-20">
-        <h2 className="text-white hover:text-yellow-400 font-bold font-custom tracking-widest">
+        <h2 className="text-2xl p-6 hidden md:block tracking-widest text-white hover:text-yellow-500 font-bold">
           HOME
         </h2>
-        <h2 className="text-white hover:text-yellow-400 font-bold font-custom tracking-widest">
+        <h2 className="text-2xl p-6 hidden md:block tracking-widest text-white hover:text-yellow-500 font-bold">
           EVENTS
         </h2>
         <img
           src={Logo}
           alt=""
-          className="flex justify-center items-center h-[60px] hover:h-[65px]"
+          className="flex justify-center items-center h-[60px] md:h-[65px] hover:h-[70px]"
         />
-        <h2 className="text-white hover:text-yellow-400 font-bold font-custom tracking-widest">
+        <h2 className="text-2xl p-6 hidden md:block tracking-widest text-white hover:text-yellow-500 font-bold">
           ABOUT
         </h2>
-        <h2 className="text-white hover:text-yellow-400 font-bold font-custom tracking-widest">
+        <h2 className="text-2xl p-6 hidden md:block tracking-widest text-white hover:text-yellow-500 font-bold">
           CONTACT
         </h2>
       </div>
